44202ab10114d928ad5cbc81f4f1474873fb68d4
Seong57 <Seong57@naver.com>
2024-04-06 17:30:04 +0900

    Edit : Modify settings

--- src/main/java/com/cclab/oauth/config/SecurityConfig.java
+++ src/main/java/com/cclab/oauth/config/SecurityConfig.java
@@ -5,18 +5,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final ObjectMapper objectMapper;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
@@ -30,13 +37,14 @@ public class SecurityConfig {
                     configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .authorizeHttpRequests(request -> {
-                    request.requestMatchers("/api/**").authenticated();
                     request.anyRequest().permitAll();
                 })
-                .oauth2Login(oauth2Configurer -> oauth2Configurer
-                        .authorizationEndpoint(cus -> cus.baseUri("/member-service/open-api/oauth2/authorize"))
-                )
                 .build();
     }
 
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
