a03ef9d70ffc82b01d3747f7a6424d3287f667b1
Seong57 <Seong57@naver.com>
2024-04-08 00:56:23 +0900

    Add : Common module

---
+++ src/main/java/com/cclab/oauth/commom/error/UserErrorCode.java
@@ -0,0 +1,17 @@
+package com.cclab.oauth.commom.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum UserErrorCode implements ErrorCodeIfs{
+
+    USER_NOT_FOUND(400, 1400, "유저를 찾을 수 없음"),
+    EXISTS_USER(400, 1401, "이미 존재하는 회원입니다."),
+    ;
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String errorDescription;
+}
