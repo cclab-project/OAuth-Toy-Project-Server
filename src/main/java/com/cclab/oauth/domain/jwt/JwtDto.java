5d317cadca632f1782d5e18e2848af23dccf2662
Seong57 <Seong57@naver.com>
2024-04-08 01:03:21 +0900

    Add : jwt token dto

--- src/main/java/com/cclab/oauth/domain/jwt/JwtDto.java
+++ src/main/java/com/cclab/oauth/domain/jwt/JwtDto.java
@@ -1,4 +1,18 @@
 package com.cclab.oauth.domain.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class JwtDto {
+
+    private String token;
+    private LocalDateTime expiredAt;
 }
